581d853e43b426c3c9484c6752f7a205fb3d409e
mdeejay <mdjrussia@gmail.com>
2019-08-15 12:40:02 +0200

    Revert "f2fs: fix to propagate error from __get_meta_page()"
    This reverts commit 050894465051b234c95f93cbfdcccd113283b534.

--- fs/f2fs/recovery.c
+++ fs/f2fs/recovery.c
@@ -259,10 +259,6 @@ static int find_fsync_dnodes(struct f2fs_sb_info *sbi, struct list_head *head,
 			return 0;
 
 		page = get_tmp_page(sbi, blkaddr);
-		if (IS_ERR(page)) {
-			err = PTR_ERR(page);
-			break;
-		}
 
 		if (!is_recoverable_dnode(page))
 			break;
@@ -477,10 +473,7 @@ retry_dn:
 
 	f2fs_wait_on_page_writeback(dn.node_page, NODE, true);
 
-	err = get_node_info(sbi, dn.nid, &ni);
-	if (err)
-		goto err;
-
+	get_node_info(sbi, dn.nid, &ni);
 	f2fs_bug_on(sbi, ni.ino != ino_of_node(page));
 	f2fs_bug_on(sbi, ofs_of_node(dn.node_page) != ofs_of_node(page));
 
@@ -583,10 +576,6 @@ static int recover_data(struct f2fs_sb_info *sbi, struct list_head *inode_list,
 		ra_meta_pages_cond(sbi, blkaddr);
 
 		page = get_tmp_page(sbi, blkaddr);
-		if (IS_ERR(page)) {
-			err = PTR_ERR(page);
-			break;
-		}
 
 		if (!is_recoverable_dnode(page)) {
 			f2fs_put_page(page, 1);
