c7bf0b3aaf466cb665483680421c0bad814e36ac
Subash Abhinov Kasiviswanathan <subashab@codeaurora.org>
2019-08-17 16:38:30 +0200

    soc: qcom: wda: Disable powersave work on deinitialization
    A delayed powersave work may not be cancelled if it re-arms itself.
    This could lead to invalid memory access after the work is destroyed.
    This change prevents the powersave work from re-queueing during
    deinitialization process.
    
    Change-Id: Ib5257c05e10e2e12c64ec28043ad0664574ccf97
    Acked-by: Weiyi Chen <weiyic@qti.qualcomm.com>
    Signed-off-by: Subash Abhinov Kasiviswanathan <subashab@codeaurora.org>
    
    Signed-off-by: mdeejay <mdjrussia@gmail.com>

--- drivers/soc/qcom/qmi_rmnet.c
+++ drivers/soc/qcom/qmi_rmnet.c
@@ -760,6 +760,7 @@ EXPORT_SYMBOL(qmi_rmnet_qos_exit);
 #ifdef CONFIG_QCOM_QMI_POWER_COLLAPSE
 static struct workqueue_struct  *rmnet_ps_wq;
 static struct rmnet_powersave_work *rmnet_work;
+static bool rmnet_work_quit;
 static struct list_head ps_list;
 
 struct rmnet_powersave_work {
@@ -842,9 +843,10 @@ EXPORT_SYMBOL(qmi_rmnet_set_powersave_mode);
 
 static void qmi_rmnet_work_restart(void *port)
 {
-	if (!rmnet_ps_wq || !rmnet_work)
-		return;
-	queue_delayed_work(rmnet_ps_wq, &rmnet_work->work, NO_DELAY);
+	rcu_read_lock();
+	if (!rmnet_work_quit)
+		queue_delayed_work(rmnet_ps_wq, &rmnet_work->work, NO_DELAY);
+	rcu_read_unlock();
 }
 
 static void qmi_rmnet_check_stats(struct work_struct *work)
@@ -871,13 +873,9 @@ static void qmi_rmnet_check_stats(struct work_struct *work)
 		qmi->ps_ignore_grant = false;
 
 		/* Register to get QMI DFC and DL marker */
-		if (qmi_rmnet_set_powersave_mode(real_work->port, 0) < 0) {
-			/* If this failed need to retry quickly */
-			queue_delayed_work(rmnet_ps_wq,
-					   &real_work->work, HZ / 50);
-			return;
+		if (qmi_rmnet_set_powersave_mode(real_work->port, 0) < 0)
+			goto end;
 
-		}
 		qmi->ps_enabled = false;
 
 		/* Do a query when coming out of powersave */
@@ -907,11 +905,9 @@ static void qmi_rmnet_check_stats(struct work_struct *work)
 			goto end;
 
 		/* Deregister to suppress QMI DFC and DL marker */
-		if (qmi_rmnet_set_powersave_mode(real_work->port, 1) < 0) {
-			queue_delayed_work(rmnet_ps_wq,
-					   &real_work->work, PS_INTERVAL);
-			return;
-		}
+		if (qmi_rmnet_set_powersave_mode(real_work->port, 1) < 0)
+			goto end;
+
 		qmi->ps_enabled = true;
 
 		/* Ignore grant after going into powersave */
@@ -929,7 +925,10 @@ static void qmi_rmnet_check_stats(struct work_struct *work)
 		return;
 	}
 end:
-	queue_delayed_work(rmnet_ps_wq, &real_work->work, PS_INTERVAL);
+	rcu_read_lock();
+	if (!rmnet_work_quit)
+		queue_delayed_work(rmnet_ps_wq, &real_work->work, PS_INTERVAL);
+	rcu_read_unlock();
 }
 
 static void qmi_rmnet_work_set_active(void *port, int status)
@@ -966,6 +965,7 @@ void qmi_rmnet_work_init(void *port)
 	rmnet_get_packets(rmnet_work->port, &rmnet_work->old_rx_pkts,
 			  &rmnet_work->old_tx_pkts);
 
+	rmnet_work_quit = false;
 	qmi_rmnet_work_set_active(rmnet_work->port, 1);
 	queue_delayed_work(rmnet_ps_wq, &rmnet_work->work, PS_INTERVAL);
 }
@@ -988,6 +988,10 @@ void qmi_rmnet_work_exit(void *port)
 {
 	if (!rmnet_ps_wq || !rmnet_work)
 		return;
+	
+	rmnet_work_quit = true;
+	synchronize_rcu();
+
 	cancel_delayed_work_sync(&rmnet_work->work);
 	destroy_workqueue(rmnet_ps_wq);
 	qmi_rmnet_work_set_active(port, 0);
